7e5a8e7ed41ffb5d2a516795694b3d42e085cf29
Eris Atienza <xxeris.a@gmail.com>
2021-09-13 18:29:27 +1200

    Added GET requests for recipe summary and similar recipes

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,5 +1,5 @@
 // Directory Structure Example
-// Feel free to delete this file. 
+// Feel free to delete this file.
 
 // This file would have methods like:
 // - getRecipeByQuery()
@@ -11,23 +11,53 @@ import fetch from "node-fetch";
 import { SPOONACULAR_API_KEY } from "../../credentials.js";
 
 export function getRecipeByQuery(query, limit) {
-  return fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${SPOONACULAR_API_KEY}&query=${query}&number=${limit}`)
-    .then(response => response.json())
-    .then(data => {
-      return data
+  return fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${SPOONACULAR_API_KEY}&query=${query}&number=${limit}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
     })
-    .catch(e => {
+    .catch((e) => {
       console.error(e);
-    })
+    });
 }
 
 export function getRecipeByID(id) {
-  return fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${SPOONACULAR_API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      return data
+  return fetch(
+    `https://api.spoonacular.com/recipes/${id}/information?apiKey=${SPOONACULAR_API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
     })
-    .catch(e => {
+    .catch((e) => {
       console.error(e);
+    });
+}
+
+export function getSimilarRecipesByID(id) {
+  return fetch(
+    `https://api.spoonacular.com/recipes/${id}/similar?apiKey=${SPOONACULAR_API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
     })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+export function getRecipeSummaryByID(id) {
+  return fetch(
+    `https://api.spoonacular.com/recipes/${id}/summary?apiKey=${SPOONACULAR_API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,46 +1,89 @@
-import express from 'express';
-import { getRecipeByQuery, getRecipeByID } from "../controller/index.js";
+import express from "express";
+import {
+  getRecipeByQuery,
+  getRecipeByID,
+  getSimilarRecipesByID,
+  getRecipeSummaryByID,
+} from "../controller/index.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.listen(
-  PORT,
-  () => console.log(`Listening on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
 // Example 1: http://localhost:8080/recipes?keyword=curry
 // Example 2: http://localhost:8080/recipes?keyword=curry&limit=5
-app.get('/recipes', (req, res) => {
+app.get("/recipes", (req, res) => {
   let keywordQuery = req.query.keyword;
   // If limit exists inside query parameter and is between 0 and 100, then use the limit in query. Otherwise, default to 10
-  let limit = (req.query.limit && req.query.limit > 0 && req.query.limit < 100) ? req.query.limit : 10;
+  let limit =
+    req.query.limit && req.query.limit > 0 && req.query.limit < 100
+      ? req.query.limit
+      : 10;
   getRecipeByQuery(keywordQuery, limit)
-    .then(response => {
+    .then((response) => {
       res.status(200).send(response);
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(400).send(error);
-    })
+    });
 });
 
 // Example 1: http://localhost:8080/recipes/650378
-app.get('/recipes/:id', (req, res) => {
+app.get("/recipes/:id", (req, res) => {
   const { id } = req.params;
   getRecipeByID(id)
-    .then(response => {
+    .then((response) => {
       if (response.status == 404) {
         res.status(404).send({
-          "status" : 404,
-          "message" : "The recipe with the id does not exist"
-        })
+          status: 404,
+          message: "The recipe with the id does not exist",
+        });
       } else {
         res.status(200).send(response);
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
+    });
+});
+
+// Example 1: http://localhost:8080/recipes/650378/similar
+app.get("/recipes/:id/similar", (req, res) => {
+  const { id } = req.params;
+  getSimilarRecipesByID(id)
+    .then((response) => {
+      if (response.status == 404) {
+        res.status(404).send({
+          status: 404,
+          message: "The recipe with the id does not exist",
+        });
+      } else {
+        res.status(200).send(response);
+      }
     })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+// Example 1: http://localhost:8080/recipes/650378/summary
+app.get("/recipes/:id/summary", (req, res) => {
+  const { id } = req.params;
+  getRecipeSummaryByID(id)
+    .then((response) => {
+      if (response.status == 404) {
+        res.status(404).send({
+          status: 404,
+          message: "The recipe with the id does not exist",
+        });
+      } else {
+        res.status(200).send(response);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
