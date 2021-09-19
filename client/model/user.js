5728e7f9e1ea4681b041610d7a3c9c3196eaecae
August Bolter <53500121+bolteraugu@users.noreply.github.com>
2021-09-20 02:44:44 +1200

    FInished registration including showing error messages to the user and a password complexity bar

--- client/model/user.js
+++ client/model/user.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-const User = mongoose.model('User', new mongoose.Schema({
+//Create User Data Model
+export const User = mongoose.model('User', new mongoose.Schema({
     email: {
         type: String,
         required: true,
@@ -14,9 +15,23 @@ const User = mongoose.model('User', new mongoose.Schema({
     }
 }));
 
-function validateUser(user) {
-    let emailValid = user.email.length >= 3 && user.email.contains('@');
-    //Password must contain at least one number, letter and special character
-    let passwordValid = user.password.contains("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"); 
-    return emailValid && passwordValid;
+export function validateUser(email, password) {
+    let emailValid = email.length >= 3 && email.includes('@'); //Emails always contain @
+    //Password must contain at least one number, letter and special character and must be at least 8 characters.
+    let passwordValid = password.match("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    //If both right return 0
+    if (emailValid && passwordValid) {
+        return 0;
+    }
+    //If both wrong return 3
+    else if (!emailValid && !passwordValid) {
+        return 3;
+    }
+    //If just one wrong then return 1 or 2 depending on if it was the email or if it was the password.
+    else if (!emailValid) {
+        return 1;
+    }
+    else {
+        return 2;
+    }
 }
