c0b95f1cda85cd2c30e7968882989b99bcfe45bf
Eris Atienza <xxeris.a@gmail.com>
2021-09-22 20:37:53 +1200

    Style homepage and update app colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      yellow: colors.amber
+      primary: {
+        dg: "#4E6E5D",
+        lg: "#4da167"
+      },
+      eggshell: "#f8f4e3",
+      secondary: {
+        pink: "#e87461",
+        brown: "#C57B57"
+      }
     },
     extend: {},
   },
