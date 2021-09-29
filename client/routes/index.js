19688be18dca4f0709f8e576682823651e5c5374
danp <danielpullon@gmail.com>
2021-09-29 15:06:54 +1300

    Removes irrelevant code

--- client/routes/index.js
+++ client/routes/index.js
@@ -58,8 +58,3 @@ router.get('/recipes/:id', (req, res) => {
 	res.render('RecipeDetails', { title: "Recipe Details", rec: selectedRecipe});
 });
 
-router.get('/cookbook/:userId', (req, res) => {
-	const {userId} = req.params;
-	res.render('Cookbook', { title: "Cookbook"});
-});
-
