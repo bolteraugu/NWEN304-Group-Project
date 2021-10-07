d44f8a55245b01e3632bfdec4290c43a0ba9994d
Ahad Rahman <rahmanahad@ecs.vuw.ac.nz>
2021-10-08 00:05:45 +1300

    login with google popup window

--- client/public/controller/login.js
+++ client/public/controller/login.js
@@ -3,6 +3,7 @@ window.addEventListener('load', init);
 
 function init() {
     login();
+    loginWithGoogle();
 }
 
 function login() {
@@ -54,4 +55,67 @@ function login() {
             console.log(error);
           });
       });
+}
+
+function loginWithGoogle() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCPoe-cWObB1rYclRbtg3DRqeKPgX16cGw",
+    authDomain: "cooked-nwen304-2021.firebaseapp.com",
+    projectId: "cooked-nwen304-2021",
+    storageBucket: "cooked-nwen304-2021.appspot.com",
+    messagingSenderId: "522804403563",
+    appId: "1:522804403563:web:bcd1b79dc2fd9df331402e",
+    measurementId: "G-ES6QRSC4XP"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  let provider = new firebase.auth.GoogleAuthProvider();
+
+  document.getElementById('googleLogin').addEventListener('click', () => {
+    firebase.auth().signInWithPopup(provider).then(res => {
+      const credential = res.credential;
+      const token = credential.accessToken;
+
+      fetch(`http://localhost:8080/signinwithgoogle`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Origin: `http://localhost:3000`,
+        },
+        body: JSON.stringify({ emailVal: res.user.email, passwordVal: res.user.uid }),
+      })
+        .then((response) => {
+          //If login was successful then redirect user back to home page (which should be different then if they visited it not logged in)
+          if (response.ok) {
+            response.json().then((data) => {
+              window.localStorage.setItem('token', data.token);
+              window.localStorage.setItem('userID', data.userID);
+              window.localStorage.setItem('cookbookID', data.cookbookID);
+              window.location.href = '/';
+            });
+          } else {
+            //Otherwise if an error occured
+            response.json().then((data) => {
+              if (data.message) {
+                //If it is one of the errors I specifically throw then show it on screen
+                document.getElementById('errorMessageLogin').innerText =
+                  data.message;
+                document.getElementById(
+                  'errorMessageLogin'
+                ).style.visibility = 'visible';
+              } else {
+                //Otherwise just log it
+                console.log(data);
+              }
+            });
+          }
+        })
+
+
+    }).catch(e=>{
+      console.log(e)
+    })
+  });
+
 }
