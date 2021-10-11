d83ed98b9136c440403602588c40aa62b6ef8649
Eris Atienza <xxeris.a@gmail.com>
2021-10-11 18:54:57 +1300

    Add function to delete user button in cookbook page

--- client/public/controller/cookbook.js
+++ client/public/controller/cookbook.js
@@ -2,24 +2,30 @@
 window.addEventListener('load', init);
 
 function init() {
-  initialRendering();
-}
-
-function initialRendering() {
-  var theDiv = document.getElementById('');
-  var content = document.createTextNode('<YOUR_CONTENT>');
-  theDiv.appendChild(content);
+    deleteUser();
 }
 
 function deleteUser() {
-  const userID = window.localStorage.getItem('userID');
-  if (!userID) throw new Error('No user is currently logged in.');
-  fetch(`http://localhost:8080/users/${userID}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Origin: `http://localhost:3000`,
-    },
-  });
+    document
+        .querySelector('#deleteUser')
+        .addEventListener('click', async function () {
+            const userID = window.localStorage.getItem('userID');
+            if (!userID) throw new Error('No user is currently logged in.');
+            await fetch(`http://localhost:8080/users/${userID}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Origin: `http://localhost:3000`,
+                },
+            }).then((response) => {
+                if (response.ok){
+                    window.localStorage.removeItem("token");
+                    initialRenderingNav();
+                    window.location.href = '/';
+                } else {
+                    console.log("Error, user not found.");
+                }
+            });
+        })
 }
