302ac16e5ccc1e58672e300e42a8cc59c34a3933
Ahad Rahman <rahmanahad@ecs.vuw.ac.nz>
2021-10-12 20:45:57 +1300

    #40: frontend displaying keywords

--- client/public/controller/searchField.js
+++ client/public/controller/searchField.js
@@ -3,6 +3,7 @@ window.addEventListener('load', init);
 
 function init() {
   search();
+  updateRecentSearches();
 }
 
 function search() {
@@ -23,4 +24,37 @@ function search() {
     .addEventListener('submit', function (event) {
       event.preventDefault();
     });
+}
+
+async function updateRecentSearches() {
+  let recentKeywords = ["spicy", "Japanese", "pizza", "dessert", "Italian", "burgers", "noodles"];
+  let userID = window.localStorage.getItem('userID');
+  if (userID) {
+    await fetch(`http://localhost:8080/users/${userID}/searches`)
+      .then((response) => response.json())
+      .then((data) => {
+        recentKeywords = data;
+        console.log(recentKeywords);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  recentKeywords.forEach((keyword) => {
+    let keywordDiv = document.createElement("div");
+    keywordDiv.className = "flex bg-secondary-brown px-2 py-1 m-1 rounded-xl hover:bg-secondary-pink shadow-md hover:shadow-lg";
+
+    let searchTerm = document.createElement("a");
+    searchTerm.className = "rounded-m searchTerm";
+    searchTerm.href = "/?search=" + keyword;
+    searchTerm.innerText = keyword;
+
+    keywordDiv.appendChild(searchTerm);
+
+    document.getElementById("recentKeywords").appendChild(keywordDiv);
+  });
+
+
+
 }
